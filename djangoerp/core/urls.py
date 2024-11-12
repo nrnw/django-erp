2f79d51c96f92dfe668cac13d9a8564ca1ec3c0e
Niuru <niurur@orise-learn.lk>
2024-11-12 21:04:31 +0530

    update urlpatterns

--- djangoerp/core/urls.py
+++ djangoerp/core/urls.py
@@ -15,9 +15,9 @@ __author__ = 'Emanuele Bertoldi <emanuele.bertoldi@gmail.com>'
 __copyright__ = 'Copyright (c) 2013 Emanuele Bertoldi'
 __version__ = '0.0.1'
 
-from django.conf.urls import patterns
+from django.urls import path
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    (r'^$', TemplateView.as_view(template_name="index.html")),
-)
+urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html"), name='home'),
+]
